8b1856c51a0e528d794cb01b0b14e34a347178b8
Eren Pekdemir <93570359+erenlifetime@users.noreply.github.com>
2024-04-19 08:03:46 +0300

    Update Beginning.swift

--- Deneme22/Beginning.swift
+++ Deneme22/Beginning.swift
@@ -1,6 +1,8 @@
 
 import Foundation
-
+// Amacım soruları kullanıcıya gösterip cevabını bulup cevapları kullanıcıya göstermesi.
+// sorularımı gösterecek bir tane text string değişkenime ihtiyaç var. 
+// Ardından cevapları ve doğru cevabı gösterecek kullanıcıya değişkene ihtiyaçım var.
 struct Question {
     let text: String
     let answers: [String]
